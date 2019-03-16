92f5f7f59c3b7af8825befffafa751fe507d8c18
Alex Merecka <merecka@gmail.com>
2019-03-16 22:55:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,17 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
+          value.each do |flavor|
+            value.delete_if {|flavor| flavor == "strawberry"}
+          end
+        end  
+     end
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
